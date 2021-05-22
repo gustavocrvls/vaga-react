b7002ddc4963b53ccfd3597b895956adf734a74d
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-05-21 23:12:47 -0300

    Melhorias de usabilidade
    ## tela: Bag
    - Mensagens de que a sacola está vazia

--- src/pages/Bag/index.tsx
+++ src/pages/Bag/index.tsx
@@ -6,7 +6,7 @@ import { BagContext } from '../../contexts/BagContext';
 import { ProductsList } from './components/ProductsList';
 
 export function Bag(): JSX.Element {
-  const { total } = useContext(BagContext);
+  const { total, items } = useContext(BagContext);
 
   return (
     <Flex
@@ -19,28 +19,45 @@ export function Bag(): JSX.Element {
           Sacola
         </Heading>
         <ProductsList />
+
+        {items.length === 0 && (
+          <Box>
+            Você ainda não comprou nada.
+            <Button
+              as={Link}
+              variant="link"
+              to="/products"
+              marginLeft="2"
+              color="#3D3D3D"
+            >
+              {` Que tal dar uma olhada nos nossos produtos?`}
+            </Button>
+          </Box>
+        )}
       </Box>
-      <Box textAlign="right">
-        <Text as="h3" fontSize="3xl">
-          Total:
-          <strong>
-            {` ${new Intl.NumberFormat('pt-br', {
-              style: 'currency',
-              currency: 'BRL',
-            }).format(total)}`}
-          </strong>
-        </Text>
-        <Button
-          as={Link}
-          to="/payment"
-          rightIcon={<FiArrowRight />}
-          type="button"
-          colorScheme="blackAlpha"
-          backgroundColor="#3D3D3D"
-        >
-          Pagamento
-        </Button>
-      </Box>
+      {items.length > 0 && (
+        <Box textAlign="right">
+          <Text as="h3" fontSize="3xl">
+            Total:
+            <strong>
+              {` ${new Intl.NumberFormat('pt-br', {
+                style: 'currency',
+                currency: 'BRL',
+              }).format(total)}`}
+            </strong>
+          </Text>
+          <Button
+            as={Link}
+            to="/payment"
+            rightIcon={<FiArrowRight />}
+            type="button"
+            colorScheme="blackAlpha"
+            backgroundColor="#3D3D3D"
+          >
+            Pagamento
+          </Button>
+        </Box>
+      )}
     </Flex>
   );
 }
